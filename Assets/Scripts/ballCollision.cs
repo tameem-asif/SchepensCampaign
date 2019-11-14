7a8372bc97f251f77b872135cb684c232870ff01
tameem-asif <tameem.asif02@gmail.com>
2019-11-14 10:55:47 -0500

    Coolio

---
+++ Assets/Scripts/ballCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if (col.gameObject.tag == "obstacle")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/bugCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bugCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if (col.gameObject.tag == "bullet")
+		{
+			Destroy(col.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}

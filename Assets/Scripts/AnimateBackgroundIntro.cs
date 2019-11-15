b0f2f295e40eb46881caa5edc7f4389e85fe85b7
Elizabeth Yang Mac <kimmy.yang.hey@gmail.com>
2019-11-15 10:35:35 -0500

    Animation Intro

---
+++ Assets/Scripts/AnimateBackgroundIntro.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimateBackgroundIntro : MonoBehaviour {
+	public Sprite[] animatedImages;
+	public Image animatedImageObj;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animatedImageObj.sprite= animatedImages[(int)(Time.time * 10)%animatedImages.Length];
+	}
+}
